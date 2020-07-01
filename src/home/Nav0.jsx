f5eb095c05528867e560fa73ade09c9d4f523b00
assskiller <674478778@qq.com>
2020-07-01 13:13:17 +0800

    添加了首页导航栏

--- src/home/Nav0.jsx
+++ src/home/Nav0.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TweenOne from 'rc-tween-one';
 import { Menu } from 'antd';
 import { getChildrenToRender } from './utils';
+import abstergo_logo from "../img/abstergo.jpg"
 
 const { Item, SubMenu } = Menu;
 
@@ -95,7 +96,11 @@ class Header extends React.Component {
             className="header0-logo"
           >
             {/* logo设置 */}
-            <img width="100%" src="https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg" alt="img" />
+            {/* <img width="100%" src={abstergo_logo} alt="img" className="ab-logo-home"/> */}
+            <div className="ab-logo-container">
+              <img src={abstergo_logo} alt="logo" className="ab-logo-img" />
+              <div className="ab-logo-text" >Abstergo</div>
+            </div>
           </TweenOne>
           {/* 手机模式下显示 */}
           {isMobile && (
@@ -134,6 +139,7 @@ class Header extends React.Component {
             <Menu
               mode={isMobile ? 'inline' : 'horizontal'}
               defaultSelectedKeys={['sub0']}
+              selectedKeys ={[]}
               theme="dark"
             >
               {navChildren}
