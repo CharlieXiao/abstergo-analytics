56e3a26e35dfdd0f917bcd91a58d1c8f43d20fa1
assskiller <674478778@qq.com>
2020-07-04 12:01:21 +0800

    调整了相关接口，加上了axios请求，具体的url还需要调整

--- src/pages/dashboard-component/DayTimeAndPriceChart.jsx
+++ src/pages/dashboard-component/DayTimeAndPriceChart.jsx
@@ -1,59 +1,78 @@
 import React, { useState, useEffect } from 'react';
 import { Line } from '@ant-design/charts';
 import { Card } from 'antd'
+import axios from 'axios';
 
-const DayTimeAndPriceChart = ()=>{
+const DayTimeAndPriceChart = () => {
 
-    const [dayTimePrice,setDayTimePrice] = useState([]);
+  const [dayTimePrice, setDayTimePrice] = useState([]);
 
-    useEffect(()=>{
-        setDayTimePrice([
-            {时间:"00:00",平均机票价格:233},
-            {时间:"01:00",平均机票价格:3233},
-            {时间:"02:00",平均机票价格:4233},
-            {时间:"03:00",平均机票价格:5233},
-            {时间:"04:00",平均机票价格:3233},
-            {时间:"05:00",平均机票价格:2233},
-            {时间:"06:00",平均机票价格:1233},
-        ])
-    },[])
 
-      const config = {
-        height:"300",
-        title: {
-          visible: false,
-          text: '配置折线数据点样式',
-        },
-        description: {
-          visible: false,
-          text: '自定义配置趋势线上数据点的样式',
-        },
-        padding: 'auto',
-        forceFit: true,
-        data:dayTimePrice,
-        xField: '时间',
-        yField: '平均机票价格',
-        label: {
-          visible: true,
-          type: 'point',
-        },
-        color: "#FF4500",
-        point: {
-          visible: true,
-          size: 5,
-          shape: 'diamond',
-          style: {
-            fill: 'white',
-            stroke: '#FF4500',
-            lineWidth: 2,
-          },
-        },
-      };
-      return(
-        <Card title="全年各市航班价格平均值">
-           <Line {...config} />
-        </Card>
-      );
+  /**
+   * 获取一天内不同时间的机票平均价格
+   */
+  const getAndSetDayTimePrice = () => {
+    axios.get("/flight/getDayTimePrice").then((res) => {
+      if (res.data.success) {
+        setDayTimePrice(res.data.data);
+      } else {
+        alert(res.data.msg);
+      }
+    }).catch((e) => {
+      let data = [
+        { 时间: "00:00", 平均机票价格: 233 },
+        { 时间: "01:00", 平均机票价格: 3233 },
+        { 时间: "02:00", 平均机票价格: 4233 },
+        { 时间: "03:00", 平均机票价格: 5233 },
+        { 时间: "04:00", 平均机票价格: 3233 },
+        { 时间: "05:00", 平均机票价格: 2233 },
+        { 时间: "06:00", 平均机票价格: 1233 },
+      ];
+      setDayTimePrice(data);
+    })
+  }
+
+
+  useEffect(() => {
+    getAndSetDayTimePrice();
+  }, [])
+
+  const config = {
+    height: "300",
+    title: {
+      visible: false,
+      text: '配置折线数据点样式',
+    },
+    description: {
+      visible: false,
+      text: '自定义配置趋势线上数据点的样式',
+    },
+    padding: 'auto',
+    forceFit: true,
+    data: dayTimePrice,
+    xField: '时间',
+    yField: '平均机票价格',
+    label: {
+      visible: true,
+      type: 'point',
+    },
+    color: "#FF4500",
+    point: {
+      visible: true,
+      size: 5,
+      shape: 'diamond',
+      style: {
+        fill: 'white',
+        stroke: '#FF4500',
+        lineWidth: 2,
+      },
+    },
+  };
+  return (
+    <Card title="全年各市航班价格平均值">
+      <Line {...config} />
+    </Card>
+  );
 }
 
 
